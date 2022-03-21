46de0bbaf96e2ab1a54ca22f013a1e0fd9cf9cf3
Hicham010 <47334105+Hicham010@users.noreply.github.com>
2022-03-21 14:53:32 +0100

    Update TradeCoinComposable

--- contracts/TradeCoinComposable.sol
+++ contracts/TradeCoinComposable.sol
@@ -9,14 +9,65 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 contract TradeCoinTokenizer is ERC721 {
     using Counters for Counters.Counter;
 
+    struct CommodityStruct {
+        CommodityState state;
+        string commodityType;
+        uint256 weightInGram;
+        string[] isoList;
+        address pickupAddress;
+        address destinationAddress;
+    }
+
+    enum CommodityState {
+        PendingConfirmation,
+        Confirmed,
+        PendingProcess,
+        Processing,
+        PendingTransport,
+        Transporting,
+        PendingStorage,
+        Stored,
+        EOL
+    }
+
     Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("TradeCoinTokenizer", "TCT") {}
 
-    function safeMint(address to) public {
+    mapping(uint256 => CommodityStruct) private Commodity;
+
+    function safeMint(
+        address to,
+        uint256 weightInGram,
+        string memory commodityType
+    ) public {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+
+        string[] memory emptyList;
+        uint256 id = tokenId;
+
+        CommodityStruct memory _commodity = CommodityStruct(
+            CommodityState.PendingConfirmation,
+            commodityType,
+            weightInGram,
+            emptyList,
+            address(0),
+            address(0)
+        );
+
+        Commodity[id] = _commodity;
+    }
+
+    function addProcess(uint256 _id, string memory _process) external {
+        require(msg.sender == ownerOf(_id), "You are not the owner");
+        Commodity[_id].isoList.push(_process);
+    }
+
+    function decreaseWeight(uint256 _id, uint256 decreaseAmount) external {
+        require(msg.sender == ownerOf(_id), "You are not the owner");
+        Commodity[_id].weightInGram -= decreaseAmount;
     }
 
     function transfer(
@@ -30,6 +81,10 @@ contract TradeCoinTokenizer is ERC721 {
         bytes memory _data = abi.encode(priceInEth, warehouse, financer, fiat);
         safeTransferFrom(msg.sender, _to, id, _data);
     }
+
+    // modifier onlySetupContract(msg.sender) {
+    //     require(msg.sender == SetupAddr)
+    // }
 }
 
 contract TradeCoinSetup is IERC721Receiver {
@@ -167,11 +222,30 @@ contract TradeCoinSetup is IERC721Receiver {
         delete tradecoindr[_id];
         emit saleReversed(_id);
     }
+
+    function addProcess(uint256 _id, string memory _process) external {
+        require(
+            tradeCoinData.ownerOf(_id) == msg.sender,
+            "You are not the owner of this data token"
+        );
+
+        tradeCoinTokenizer.addProcess(_id, _process);
+    }
+
+    function decreaseWeight(uint256 _id, uint256 decreaseAmount) external {
+        require(
+            tradeCoinData.ownerOf(_id) == msg.sender,
+            "You are not the owner of this data token"
+        );
+
+        tradeCoinTokenizer.decreaseWeight(_id, decreaseAmount);
+    }
 }
 
 contract TradeCoinRights is ERC721 {
     using Counters for Counters.Counter;
 
+    TradeCoinSetup tradeCoinSetup;
     address public tradeCoinSetupAddr;
     Counters.Counter private _tokenIdCounter;
 
@@ -193,6 +267,7 @@ contract TradeCoinRights is ERC721 {
     }
 
     function setTradeCoinSetupAddr(address _tradeCoinSetupAddr) public {
+        tradeCoinSetup = TradeCoinSetup(_tradeCoinSetupAddr);
         tradeCoinSetupAddr = _tradeCoinSetupAddr;
     }
 }
@@ -200,6 +275,7 @@ contract TradeCoinRights is ERC721 {
 contract TradeCoinData is ERC721 {
     using Counters for Counters.Counter;
 
+    TradeCoinSetup tradeCoinSetup;
     address public tradeCoinSetupAddr;
     Counters.Counter private _tokenIdCounter;
 
@@ -221,6 +297,162 @@ contract TradeCoinData is ERC721 {
     }
 
     function setTradeCoinSetupAddr(address _tradeCoinSetupAddr) public {
+        tradeCoinSetup = TradeCoinSetup(_tradeCoinSetupAddr);
         tradeCoinSetupAddr = _tradeCoinSetupAddr;
     }
+
+    function addProcess(uint256 _id, string memory _process) external {
+        require(
+            ownerOf(_id) == msg.sender,
+            "You are not the owner of this data token"
+        );
+
+        tradeCoinSetup.addProcess(_id, _process);
+    }
+
+    function decreaseWeight(uint256 _id, uint256 decreaseAmount) external {
+        require(
+            ownerOf(_id) == msg.sender,
+            "You are not the owner of this data token"
+        );
+
+        tradeCoinSetup.decreaseWeight(_id, decreaseAmount);
+    }
+}
+
+contract TradeCoinSale is IERC721Receiver {
+    TradeCoinTokenizer tradeCoinTokenizer;
+    address public tradeCoinTokenizerAddr;
+
+    struct TradeCoinSaleData {
+        address seller;
+        address receiver;
+        uint256 priceInWei;
+        bool fiat;
+        bool isPayed;
+    }
+
+    constructor(address _tradeCoinTokenizerAddr) {
+        tradeCoinTokenizerAddr = _tradeCoinTokenizerAddr;
+        tradeCoinTokenizer = TradeCoinTokenizer(_tradeCoinTokenizerAddr);
+    }
+
+    mapping(uint256 => TradeCoinSaleData) private tradeCoinSaleData;
+    mapping(uint256 => bool) withdrawnPayment;
+    mapping(uint256 => bool) withdrawnToken;
+
+    event SetupSale(
+        address indexed seller,
+        address indexed receiver,
+        uint256 indexed tokenId,
+        uint256 priceInWei
+    );
+    event SetupSale(
+        address indexed seller,
+        address indexed receiver,
+        uint256 indexed tokenId,
+        bool fiat
+    );
+    event DepositPayment(address indexed depositer, uint256 indexed tokenId);
+    event WithdrawPayment(address indexed withdrawer, uint256 indexed tokenId);
+    event WithdrawToken(address indexed withdrawer, uint256 indexed tokenId);
+
+    function onERC721Received(
+        address _seller,
+        address,
+        uint256 _id,
+        bytes memory _data
+    ) public virtual override returns (bytes4) {
+        require(
+            tradeCoinTokenizerAddr == msg.sender,
+            "This is not the address of the TradeCoin Tokenizer"
+        );
+
+        (uint256 _priceInWei, address _receiver, bool _fiat) = abi.decode(
+            _data,
+            (uint256, address, bool)
+        );
+        // bool _isPayed = _fiat ? true : false;
+        if (_fiat) {
+            emit SetupSale(_seller, _receiver, _id, true);
+            tradeCoinSaleData[_id] = TradeCoinSaleData(
+                _seller,
+                _receiver,
+                0,
+                true,
+                true
+            );
+            withdrawnPayment[_id] = true;
+        } else {
+            emit SetupSale(_seller, _receiver, _id, _priceInWei);
+            tradeCoinSaleData[_id] = TradeCoinSaleData(
+                _seller,
+                _receiver,
+                _priceInWei,
+                false,
+                false
+            );
+            withdrawnPayment[_id] = false;
+        }
+
+        withdrawnToken[_id] = false;
+
+        return this.onERC721Received.selector;
+    }
+
+    function payForToken(uint256 _id) external payable {
+        TradeCoinSaleData memory _tradeCoinSaleData = dataOf(_id);
+        require(!_tradeCoinSaleData.isPayed, "This token is already payed for");
+        require(
+            (_tradeCoinSaleData.priceInWei) == msg.value,
+            "This is not the right amount"
+        );
+
+        emit DepositPayment(msg.sender, _id);
+    }
+
+    function withdrawPayment(uint256 _id) external {
+        TradeCoinSaleData memory _tradeCoinSaleData = dataOf(_id);
+        require(
+            !withdrawnPayment[_id],
+            "The payment has already been withdrawn"
+        );
+        require(!_tradeCoinSaleData.fiat, "This token has been payed in fiat");
+        require(_tradeCoinSaleData.isPayed, "This token is not payed for");
+        _tradeCoinSaleData.isPayed = false;
+        withdrawnPayment[_id] = true;
+        payable(_tradeCoinSaleData.seller).transfer(
+            _tradeCoinSaleData.priceInWei
+        );
+        emit WithdrawPayment(msg.sender, _id);
+    }
+
+    function withdrawToken(uint256 _id) external {
+        TradeCoinSaleData memory _tradeCoinSaleData = dataOf(_id);
+        require(!withdrawnToken[_id], "The token has already been withdrawn");
+        require(_tradeCoinSaleData.isPayed, "This token is not payed for");
+        withdrawnToken[_id] = true;
+
+        tradeCoinTokenizer.safeTransferFrom(
+            address(this),
+            _tradeCoinSaleData.receiver,
+            _id
+        );
+        emit WithdrawToken(msg.sender, _id);
+    }
+
+    function dataOf(uint256 _id)
+        public
+        view
+        returns (TradeCoinSaleData memory)
+    {
+        TradeCoinSaleData memory _tradeCoinSaleData = tradeCoinSaleData[_id];
+        require(_tradeCoinSaleData.seller != address(0), "Token not received");
+        return _tradeCoinSaleData;
+    }
+
+    // function setTradeCoinSetupAddr(address _tradeCoinTokenizerAddr) public {
+    //     tradeCoinTokenizerAddr = _tradeCoinTokenizerAddr;
+    //     tradeCoinTokenizer = TradeCoinTokenizer(_tradeCoinTokenizerAddr);
+    // }
 }
